d9a5a3b8760a9b13a122b1ea9569ceb53b8984c7
Onemaeus97 <benbenleroiroi1@gmail.com>
2021-10-03 12:00:33 +0200

    change icon

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,21 +12,16 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+        <h2>Yifan WANG</h2>
+        <p><a href="ywang4@etud.insa-toulouse.fr">ywang4@etud.insa-toulouse.fr</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-        I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-        the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
-        at <a href="https://matroid.com">Matroid</a>
-        , <a href="https://planet.com">Planet</a>
-        , <a href="https://planetaryresources.com">Planetary Resources</a>
-        , <a href="https://facebook.com">Facebook</a>
-        , and <a href="https://seds.org">SEDS</a>.
+      <p>Hi, I&apos;m Yifan. I am a new bee in the world of information.
+        I&apos;m currently a student specialized in CS at  <a href="https://www.insa-toulouse.fr/fr/index.html/">INSA Toulouse</a>,
+        I have also studied 2 years at <a href="https://www.esigelec.fr/fr">ESIGELEC</a> and am a <a href="https://en.wikipedia.org/wiki/Hangzhou_Foreign_Languages_School/">HFLS alumini</a>.
       </p>
       <ul className="actions">
         <li>
@@ -37,7 +32,7 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Yifan WANG <Link to="/">ywang4@etud.insa-toulouse.fr</Link>.</p>
     </section>
   </section>
 );
